88b4bc8236ab0c3eed2846dcad3aeaccb80a24ab
tyisyustat24 <tyis.yustat24@icloud.com>
2021-10-25 09:07:46 -0700

    did everything i needed to

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 console.log("hello I am alive")
 
-var numb = 7 
-var numb2 = 4
-console.log(numb + numb2);
+var Myname = "Tyis";
+let Myage = "15"
+const numb = 3.24e4;
+// var numb = 5;
+// var numb2 = 3;
+// console.log(name);
+// console.log(numb + numb2);
 
 function addition(numb1, numb2){
-    console.log(numb1 + numb2)
+    console.log(numb1 + numb2);
 }
+addition(numb, numb);
+// addition(243, 71);
+
+// function introduction(name, age) {
+ //   console.log("hello my name is") + name + " and I am fifteen yars old"
+// } 
+
+// introduction ("Tyis", "15")
